10f2d9e4745b59e4f34d9d939d4edeab915b1297
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-04-09 23:03:45 +0500

    1. learned about c types and memory allocation for different c types.

--- unix/learning-c/app.c
+++ unix/learning-c/app.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
-    int a = 2;
-    int b = 3;
-    int sum = a + b;
-    float b = 234.234;
-    char a = 'faheem';
-    fprintf(stdout, "This is a %d string", sum);
+// int add(int a, int b) { return a + b; }
 
-    return 0;
+int main() {
+  // int , size_t, char, float
+  //   int a = 2;
+  //   int b = 3;
+  //   int c = add(a, b);
+  // float b = 234.234;
+  // fprintf(stdout, "This is a %d string", sum);
+
+  //   char my_character = "g";
+
+  // size_t t = 0 18...
+
+  //   fprintf(stdout, "Size of a long int value is : %zu bytes.\n", sizeof(long
+  //   int));
+
+  size_t t = 18446744073709551615;
+
+  fprintf(stdout, "t is %zu.\n", t);
+
+  return 0;
 }
