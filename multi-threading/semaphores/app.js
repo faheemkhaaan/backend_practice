753c776eed914d5181a488f454b659011f8a1458
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-04-19 23:36:32 +0500

    1. learned about semaphores.

---
+++ multi-threading/semaphores/app.js
@@ -0,0 +1,24 @@
+const { Worker } = require("worker_threads");
+const { Buffer } = require("buffer");
+
+process.title = 'spinlock'
+const number = Buffer.from(new SharedArrayBuffer(4));
+const seal = new SharedArrayBuffer(4);
+const THREADS = 4;
+let completed = 0;
+
+
+for (let i = 0; i < THREADS; i++) {
+    const worker = new Worker('./calc.js', {
+        workerData: { number: number.buffer, seal }
+    });
+
+    worker.on("exit", () => {
+        completed++;
+        if (completed === THREADS) {
+            console.log("Final number is: ", number.readUInt32LE().toLocaleString());
+        }
+    })
+};
+
+console.log('Final result should be: ', (THREADS * 5_000_000).toLocaleString())
