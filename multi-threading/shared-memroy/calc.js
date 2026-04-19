f5dddffa6535767c8061fed8d59b32dc5f7c1ea1
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-04-19 13:18:50 +0500

    1. learend about shared memroy and how worker thread can access the same memory and mutate it.

---
+++ multi-threading/shared-memroy/calc.js
@@ -0,0 +1,11 @@
+
+const { workerData, threadId } = require('worker_threads');
+
+const data = Buffer.from(workerData.data);
+const data2 = Buffer.from(workerData.data2);
+
+
+console.log(`Thread ${threadId} data:`, data);
+
+data[threadId] = 255;
+data2[threadId] = 120;
